3c146e1eb95508ff1e2f3c28224d8421d68bf17f
parham <p.vafaeei@gamil.com>
2022-09-05 15:36:16 +0430

    create Runner and question conditions in runner ,create start method with menu and main method

--- src/main/java/permutation/Runner.java
+++ src/main/java/permutation/Runner.java
@@ -5,21 +5,31 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Runner  {
+    Permutation permutation = new Permutation();
+    Scanner scanner =new Scanner(System.in);
 
-
-    public static void main(String[] args) {
-        Permutation permutation = new Permutation();
-        Scanner scanner =new Scanner(System.in);
+    void start() {
         String input;
         System.out.println("give your String !");
         input = scanner.next();
+        if (permutation.getMemory().containsValue(input)) {
+            System.out.println("fail" +
+                    "\n" +
+                    "already given  !"+"\n  try again!");
+            start();
+        }
         HashMap<Integer, String> cases = permutation.permutation(input);
         Permutation.print(cases);
-        System.out.println("wanna go !\n 1-yes\n 2-no");
+
+        System.out.println("pass!\nwanna go !\n 1-yes\n 2-no");
         input = scanner.next().toLowerCase();
-        if (input == String.valueOf(1)) {
-            main();
+        if (input.equals(String.valueOf(1))) {
+            start();
         } else System.exit(2);
+    }
+    public static void main(String[] args) {
+        Runner runner=new Runner();
+        runner.start();
     }
     }
 
