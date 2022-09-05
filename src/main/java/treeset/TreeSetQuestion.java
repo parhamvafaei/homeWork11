c2aea1717304ade297bb64c5108f03c05900a5f1
parham <p.vafaeei@gamil.com>
2022-09-05 19:04:50 +0430

    update TreeSetQuestion

--- src/main/java/treeset/TreeSetQuestion.java
+++ src/main/java/treeset/TreeSetQuestion.java
@@ -4,31 +4,42 @@ import java.util.Random;
 import java.util.TreeSet;
 
 public class TreeSetQuestion {
-  public TreeSet<Character> random() {
-    TreeSet<Character> treeSet =new TreeSet<>() ;
-    Random r = new Random();
-      for (int i = 0; i < 10; i++) {
-        char char1 = (char) (r.nextInt(26) + 'A');
-        treeSet.add(char1);
-        System.out.println("first index :"+char1);
-      }
-      return treeSet;
+
+
+    public static TreeSet<Character> random() {
+        TreeSet<Character> set = new TreeSet<>();
+        Random random = new Random();
+        while (set.size() < 10) {
+            set.add((char) random.nextInt(97, 123));
+
+        }print(set);
+        return set;
     }
 
-    static void union(TreeSet<Character> set1 , TreeSet<Character> set2){
-    set1.addAll(set2);
+    static void print(TreeSet<Character> treeSet) {
+        System.out.println(treeSet);
+    }
+
+    static void union(TreeSet<Character> set1, TreeSet<Character> set2) {
+        TreeSet<Character> treeSet=new TreeSet<>(set1);
+        treeSet.addAll(set2);
+        print(treeSet);
 
     }
 
-    static void intersection(TreeSet<Character> set1 , TreeSet<Character> set2){
-    set1.retainAll(set2);
+    static void intersection(TreeSet<Character> set1, TreeSet<Character> set2) {
+        TreeSet<Character> treeSet=new TreeSet<>(set1);
+  treeSet.retainAll(set2);
+        print(treeSet);
     }
 
-  public static void main(String[] args) {
-    TreeSetQuestion treeSetQuestion=new TreeSetQuestion();
-    TreeSet<Character> treeSet1=new TreeSet<>();
-    TreeSet<Character> treeSet2=new TreeSet<>();
-    treeSetQuestion.random();
-  }
+    public static void main(String[] args) {
+
+        TreeSet<Character> treeSet1 = new TreeSet<>(random());
+        TreeSet<Character> treeSet2 = new TreeSet<>(random());
+   union(treeSet1,treeSet2);
+   intersection(treeSet1,treeSet2);
+
+    }
 
 }
